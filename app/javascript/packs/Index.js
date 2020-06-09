0aee5f7f05760199adb73c625b68dd7b8d1c3670
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-06-09 19:22:41 -0300

    entry component

---
+++ app/javascript/packs/Index.js
@@ -0,0 +1,16 @@
+//react
+import React from "react";
+import { render } from "react-dom";
+//boostratp
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+//components
+import App from "../components/App";
+
+document.addEventListener("DOMContentLoaded", () => {
+  render(
+    <App />,
+    document.body.appendChild(document.createElement("div"))
+  );
+})
