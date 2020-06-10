19a76098f6fc0170637226c28c12d8f898e1ca50
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-06-10 20:26:49 -0300

    configuring animals routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :animal do
+      get 'animals/index'
+      post 'animals/create'
+      get '/show/:id', to: 'animals#show'
+      get '/destroy/:id', to: 'animals#destroy'
+    end
+  end
   root 'home#index'
+  get '/*path' => 'home#index' # redirect requests that doesn’t match the existing routes to home index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
