31fc42186a99bc73d8f3f6933bc148db4566a7e5
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-06-13 21:51:24 -0300

    create method

--- app/controllers/api/animal/animals_controller.rb
+++ app/controllers/api/animal/animals_controller.rb
@@ -3,11 +3,24 @@ class Api::Animal::AnimalsController < ApplicationController
   end
 
   def create
+    animal = Animal.create!(animal_params)
+
+    if animal
+      render json: animal
+    else
+      render json: animal.errors
+    end
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+
+  def animal_params
+    params.permit(:name, :birth, :species, :breed)
   end
 end
