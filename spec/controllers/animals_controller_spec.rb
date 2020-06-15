c878db5ea09c9851f1e2d588eb3b142383127cb8
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-06-15 12:44:40 -0300

    tests for create method in controller

--- spec/controllers/animals_controller_spec.rb
+++ spec/controllers/animals_controller_spec.rb
@@ -1,8 +1,31 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Api::Animal::AnimalsController, :type => :controller do
-    it "responds successfully" do
-        # get :index
-        expect(200).to eq(200)
+    describe "POST create" do 
+        it "create animal successfully with valid attributes" do
+            attrs = attributes_for(:animal)
+            post :create, params: attrs
+            expect(response).to have_http_status(:ok)
+        end
+
+        it "throw error with invalid name" do
+            animal = attributes_for(:animal, name: nil)
+            expect { post :create, params: animal }.to raise_error(ActiveRecord::RecordInvalid)
+        end
+
+        it "throw error with invalid birth" do
+            animal = attributes_for(:animal, birth: nil)
+            expect { post :create, params: animal }.to raise_error(ActiveRecord::RecordInvalid)
+        end
+
+        it "throw error with invalid species" do
+            animal = attributes_for(:animal, species: nil)
+            expect { post :create, params: animal }.to raise_error(ActiveRecord::RecordInvalid)
+        end
+
+        it "throw error with invalid breed" do
+            animal = attributes_for(:animal, breed: nil)
+            expect { post :create, params: animal }.to raise_error(ActiveRecord::RecordInvalid)
+        end
     end
 end
