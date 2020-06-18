3d9813f94f6779bd2dd3f6eaf2bbe4ef2b2bfb4e
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-06-18 12:40:27 -0300

    index and update methods

--- app/controllers/api/animal/animals_controller.rb
+++ app/controllers/api/animal/animals_controller.rb
@@ -1,5 +1,7 @@
 class Api::Animal::AnimalsController < ApplicationController
   def index
+    animals = Animal.all.order(created_at: :desc)
+    render json: animals
   end
 
   def create
@@ -16,11 +18,24 @@ class Api::Animal::AnimalsController < ApplicationController
     if animal
       render json: animal
     else
-      render animal.errors
+      render json: animal.errors
     end
   end
 
   def destroy
+    animal&.destroy
+    render json: { message: 'animal deleted!' }
+  end
+
+  def update
+    animal = Animal.find(params[:id])
+    animal.update!(animal_params)
+
+    if animal
+      render json: animal
+    else
+      render json: animal.errors
+    end
   end
 
   private
