701c38af1a9282eb85cc11cb611b7e53b372fca9
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-06-18 12:41:46 -0300

    refactor in tests: - refactor in create and get - create delete tests - create update tests

--- spec/controllers/animals_controller_spec.rb
+++ spec/controllers/animals_controller_spec.rb
@@ -1,43 +1,93 @@
 require 'rails_helper'
+require 'date'
 
 RSpec.describe Api::Animal::AnimalsController, :type => :controller do
-    describe "POST create" do 
+    let(:animal) { create(:animal) }
+    let(:attrs_animal) { attributes_for(:animal) }
+
+    def get_value_from_json(key)
+        JSON.parse(response.body)[key]
+    end
+
+    describe "GET #index" do
+        it "find all animals" do
+            get :index
+            expect(response.body).to_not be_empty
+            expect(response).to have_http_status(:ok)
+        end
+    end
+
+    describe "POST #create" do 
         it "create animal successfully with valid attributes" do
-            attrs = attributes_for(:animal)
-            post :create, params: attrs
+            post :create, params: attrs_animal
             expect(response).to have_http_status(:ok)
         end
 
         it "throw error with invalid name" do
-            animal = attributes_for(:animal, name: nil)
-            expect { post :create, params: animal }.to raise_error(ActiveRecord::RecordInvalid)
+            attrs_animal[:name] = nil
+            expect { post :create, params: attrs_animal }.to raise_error(ActiveRecord::RecordInvalid)
         end
 
         it "throw error with invalid birth" do
-            animal = attributes_for(:animal, birth: nil)
-            expect { post :create, params: animal }.to raise_error(ActiveRecord::RecordInvalid)
+            attrs_animal[:birth] = nil
+            expect { post :create, params: attrs_animal }.to raise_error(ActiveRecord::RecordInvalid)
         end
 
         it "throw error with invalid species" do
-            animal = attributes_for(:animal, species: nil)
-            expect { post :create, params: animal }.to raise_error(ActiveRecord::RecordInvalid)
+            attrs_animal[:species] = nil
+            expect { post :create, params: attrs_animal }.to raise_error(ActiveRecord::RecordInvalid)
         end
 
         it "throw error with invalid breed" do
-            animal = attributes_for(:animal, breed: nil)
-            expect { post :create, params: animal }.to raise_error(ActiveRecord::RecordInvalid)
+            attrs_animal[:breed] = nil
+            expect { post :create, params: attrs_animal }.to raise_error(ActiveRecord::RecordInvalid)
         end
     end
 
-    describe "GET requests" do
+    describe "GET #show" do
         it "find existing animal with success" do
-            animal = create(:animal)
             get :show, params: { id: animal.id }
             expect(response.body).not_to be_empty
         end
 
         it "throw error when animal doesnt exist" do
-            expect { get :show, params: { id: -1 } }.to raise_error(ActiveRecord::RecordNotFound)
+            animal.id = -1
+            expect { get :show, params: { id: animal.id } }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+    end
+
+    describe "GET #destroy" do
+        it "destroy existing animal" do
+            delete :destroy, params: { id: animal.id }
+            expect(response.body).to eq('{"message":"animal deleted!"}')
+        end
+
+        it "throw error when animal doesnt exist" do
+            animal.id = -1
+            expect { delete :destroy, params: { id: animal.id } }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+    end
+
+    describe "PUT #update" do
+        it "update animal name with success" do
+            put :update, params: { id: animal.id, name: "luna fernandes" }
+            expect(get_value_from_json("name")).to_not eql(animal.name)
+        end
+
+        it "update animal birth with success" do
+            put :update, params: { id: animal.id, birth: "2006-07-09" }
+            updated_birth = Date.parse(get_value_from_json("birth"))
+            expect(updated_birth).to_not eql(animal.birth)
+        end
+
+        it "update animal species with success" do
+            put :update, params: { id: animal.id, species: "doggo" }
+            expect(get_value_from_json("species")).to_not eql(animal.species)
+        end
+
+        it "update animal breed with success" do
+            put :update, params: { id: animal.id, breed: "cocker spainel" }
+            expect(get_value_from_json("breed")).to_not eql(animal.breed)
         end
     end
 end
