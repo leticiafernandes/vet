0930842f1df35654bc040bd0ba61436f23f077f7
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-06-18 12:42:55 -0300

    refactor model tests

--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,31 +1,29 @@
 require "rails_helper"
 
 RSpec.describe Animal, :type => :model do
-    before do
-        @animal = build(:animal)
-    end
+    let(:animal) { create(:animal) }
 
     it "is valid with attributes" do
-        expect(@animal).to be_valid
+        expect(animal).to be_valid
     end
 
     it "is not valid without a name" do
-        @animal.name = nil
-        expect(@animal).to_not be_valid
+        animal.name = nil
+        expect(animal).to_not be_valid
     end
 
     it "is not valid without a birth" do
-        @animal.birth = nil
-        expect(@animal).to_not be_valid
+        animal.birth = nil
+        expect(animal).to_not be_valid
     end
 
     it "is not valid without a species"do
-        @animal.species = nil
-        expect(@animal).to_not be_valid
+        animal.species = nil
+        expect(animal).to_not be_valid
     end
     
     it "is not valid without a breed"do
-        @animal.breed = nil
-        expect(@animal).to_not be_valid
+        animal.breed = nil
+        expect(animal).to_not be_valid
     end
 end
