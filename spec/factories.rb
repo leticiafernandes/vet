bbfdd2a5cde2ca107422b42092843c9d3ee61509
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-06-20 15:46:05 -0300

    adding tutor facotru

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,16 @@
 FactoryBot.define do
-    factory :animal do
-        name { "luna" }
-        birth { "2005-01-01" }
-        species { "dog" }
-        breed { "cocker" }
-    end
+  factory :tutor do
+    name { "leticia" }
+    identification { "00000000000" }
+    email { "leticia@email.com" }
+    phone { "00000000000" }
+  end
+
+  factory :animal do
+      name { "luna" }
+      birth { "2005-01-01" }
+      species { "dog" }
+      breed { "cocker" }
+  end
 end
   
