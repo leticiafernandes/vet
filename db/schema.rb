b84c2372a0975b3cf6b523d87540f6adc8126a23
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-07-09 14:56:24 -0300

    adding association to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_155951) do
+ActiveRecord::Schema.define(version: 2020_06_20_205034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_06_18_155951) do
     t.string "breed", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "tutor_id"
+    t.index ["tutor_id"], name: "index_animals_on_tutor_id"
   end
 
   create_table "tutors", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema.define(version: 2020_06_18_155951) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "animals", "tutors"
 end
